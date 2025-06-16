f72c7aa5d345d8e47b13b01ecf6c1dd21ae4bf96
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-16 19:31:00 +0800

    added hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <p>search</p>
+      </div>
+    </main>
+  );
+};
 
 export default App;
